ef2e463944787fc2c4d7fe8c9b9ea39cfd57ff1d
ekasc <ekas.esc@gmail.com>
2023-09-28 16:19:17 +0530

    changes: html & svelte tags, less blue

--- lua/lush_theme/kissland.lua
+++ lua/lush_theme/kissland.lua
@@ -1,19 +1,18 @@
 -- require lush
-local lush = require('lush')
+local lush = require "lush"
 local hsl = lush.hsl
-
 -- colors
-local black = hsl('#000000')
-local electricRed = hsl('#FF0000')
-local electricBlue = hsl('#0066FF')
-local cerulean = hsl('#007BA7')
-local skyBlue = hsl('#87CEEB')
-local limeGreen = hsl('#00FF00')
-local electricGreen = hsl('#00FF33')
-local mintGreen = hsl('#98FF98')
-local tealGreen = hsl('#008080')
-local ghostWhite = hsl('#F8F8FF')
-local amber = hsl('#FFBF00')
+local black = hsl "#000000"
+local grey = black.lighten(50)
+local red = hsl "#FF0000"
+local electricBlue = hsl "#0066FF"
+local cerulean = hsl "#007BA7"
+local skyBlue = hsl "#87CEEB"
+local limeGreen = hsl "#00FF00"
+local mintGreen = hsl "#98FF98"
+local tealGreen = hsl "#008080"
+local white = hsl "#F8F8FF"
+local amber = hsl "#FFBF00"
 local darkBackground = electricBlue.darken(90)
 
 local bg = darkBackground
@@ -22,97 +21,105 @@ local bg = darkBackground
 
 -- directory and prompt
 
+local theme = lush(function(injected_functions)
+	local sym = injected_functions.sym
 
-local theme = lush(function()
 	return {
-		Normal { bg = bg, fg = ghostWhite },          -- normal text
-		Comment { fg = black.lighten(50), gui = 'italic' }, -- any comment
+		Normal { fg = white }, -- normal text
+		Comment { fg = black.lighten(50), gui = "italic" }, -- any comment
+
+		NormalFloat { fg = white },
 
 		VertSplit { fg = Comment.fg.darken(25) },
-		StatusLineNC { Comment, fg = Comment.fg.darken(25), gui = 'underline' },
-		StatusLine { Comment, fg = Comment.fg.darken(25), gui = 'underline' },
-		LineNr { StatusLineNC, gui = 'nocombine' },
-		CursorLineNr { StatusLine, gui = 'nocombine' },
+		StatusLineNC { fg = Comment.fg.darken(25) },
+		StatusLine { Comment, fg = Comment.fg.darken(25), gui = "underline" },
+		LineNr { StatusLineNC, gui = "nocombine" },
+		CursorLineNr { StatusLine, gui = "nocombine" },
 
 		ColorColumn { bg = bg },
 		CursorColumn {},
 		CursorLine {},
 
-		Folded { fg = ghostWhite, gui = 'bold' },
+		Folded { fg = white, gui = "bold" },
 		FoldColumn { Normal, fg = Folded.fg },
 		SignColumn { FoldColumn },
 
-		Pmenu { fg = ghostWhite, bg = bg.darken(50) },
-		PmenuSel { fg = Pmenu.bg, bg = Pmenu.fg },
-
 		Conceal { Normal },
-		Directory { fg = cerulean },
+		Directory { fg = cerulean.lighten(10) },
 
 		DiffAdd { fg = black, bg = limeGreen },
 		DiffChange { fg = black, bg = amber },
-		DiffDelete { fg = black, bg = electricRed },
+		DiffDelete { fg = black, bg = red },
+
+		Pmenu { fg = white },
+		PmenuSel { DiffAdd },
+		PmenuSbar { bg = grey },
+		PmenuThumb { bg = white },
 
 		Title { fg = skyBlue },
-		Visual { fg = mintGreen, bg = black.lighten(20) },
-		MatchParen { fg = electricRed, bg = black, gui = 'underline' },
+		Visual { fg = black, bg = skyBlue },
+		MatchParen { fg = red, gui = "underline" },
 
-		MsgLine { gui = 'italic' },
+		MsgLine { gui = "italic" },
 		MoreMsg { fg = skyBlue },
-		Question { MoreMsg, gui = 'nocombine' },
-		ErrorMsg { fg = electricRed, gui = 'boldunderline' },
+		Question { MoreMsg, gui = "nocombine" },
+		ErrorMsg { fg = red, gui = "boldunderline" },
 		WarningMsg { fg = amber },
 
-		NonText { fg = Visual.fg },
+		NonText { fg = mintGreen },
 		EndOfBuffer { fg = bg.lighten(10) },
-		Search { fg = black, bg = skyBlue.lighten(25), gui = 'italicboldunderline' },
+		Search { fg = black, bg = skyBlue.lighten(25), gui = "italicboldunderline" },
 		SpecialKey { fg = cerulean },
 
 		--
 
-		Constant { fg = electricRed },
-		String { fg = ghostWhite.darken(10) },
+		Constant { fg = red },
+		String { fg = white.darken(10) },
 		Character { String },
 		Number { fg = amber },
-		Boolean { Number },
+		Boolean { fg = red },
 		Float { Number },
 
 		Identifier { fg = mintGreen.darken(10) },
-		Function { gui = 'bold', fg = electricRed.lighten(15) },
+		Function { gui = "bold", fg = red.lighten(15) },
+		Method { Function },
 
-		Statement { fg = tealGreen.lighten(20), gui = 'bold' },
-		Conditional { fg = electricBlue, gui = 'italic' },
-		Repeat { Conditional },
+		Statement { fg = tealGreen.lighten(20), gui = "bold" },
 		Label { Statement },
+		Punctuation { Statement },
 		Operator { Statement },
 		Keyword { Statement },
-		Exeception { Statement },
+		Conditional { fg = electricBlue, gui = "italic" },
+		Repeat { Conditional },
+		Exeception { Conditional },
 
 		PreProc { fg = cerulean },
-		Type { PreProc },
-		Special { fg = cerulean.lighten(10), gui = 'bold' },
-		Error { fg = ghostWhite, bg = electricRed, gui = 'boldunderline' },
-		Todo { fg = black, bg = electricGreen },
+		Type { fg = red },
+		Special { fg = cerulean.lighten(10), gui = "bold" },
+		SpecialChar { fg = cerulean.lighten(50), gui = "bold" },
+		Error { fg = white, bg = red, gui = "boldunderline" },
+		Todo { fg = black, bg = limeGreen },
 
 		-- LSP
 
-		DiagnosticError { fg = electricRed.saturate(20) },
+		DiagnosticError { fg = red.saturate(20) },
 		DiagnosticWarn { fg = amber.saturate(20) },
 		DiagnosticHint { fg = skyBlue.saturate(50) },
-		DiagnosticInfo { fg = ghostWhite },
+		DiagnosticInfo { fg = white },
 
 		DiagnosticSignError { DiagnosticError },
 		DiagnosticSignWarn { DiagnosticWarn },
 		DiagnosticSignHint { DiagnosticHint },
 		DiagnosticSignInfo { DiagnosticInfo },
-		DiagnosticVirtualTextError { fg = DiagnosticError.fg.lighten(25), gui = 'bold' },
-		DiagnosticVirtualTextWarn { fg = DiagnosticWarn.fg.lighten(25), gui = 'bold' },
+		DiagnosticVirtualTextError { fg = DiagnosticError.fg.lighten(25), gui = "bold" },
+		DiagnosticVirtualTextWarn { fg = DiagnosticWarn.fg.lighten(25), gui = "bold" },
 		DiagnosticVirtualTextHint { fg = DiagnosticHint.fg.lighten(25) },
 		DiagnosticVirtualTextInfo { fg = DiagnosticInfo.fg.lighten(25) },
 
-		DiagnosticUnderlineError { gui = 'underline' },
-		DiagnosticUnderlineWarn { gui = 'underline' },
-		DiagnosticUnderlineHint { gui = 'underline' },
-		DiagnosticUnderlineInfo { gui = 'underline' },
+		DiagnosticUnderlineError { gui = "underline" },
+		DiagnosticUnderlineWarn { gui = "underline" },
+		DiagnosticUnderlineHint { gui = "underline" },
+		DiagnosticUnderlineInfo { gui = "underline" },
 
 		DiagnosticFloatError { DiagnosticVirtualTextError },
 		DiagnosticFloatWarn { DiagnosticVirtualTextWarn },
@@ -120,22 +127,22 @@ local theme = lush(function()
 		DiagnosticFloatInfo { DiagnosticVirtualTextInfo },
 
 		-- Treesitter
-		TSKeyword { Conditional },
-		TSKeywordFunction { PreProc },
-		TSFuncBuiltin { PreProc },
-		TSField {},
+		-- TSKeyword { Conditional },
+		-- TSKeywordFunction { PreProc },
+		-- TSFuncBuiltin { PreProc },
+		-- TSField {},
 
 		-- Telescope highlighting (borders are ugly by default)
 		TelescopeNormal { Normal },
-		TelescopeMatching { Identifier, fg = cerulean.lighten(20), gui = 'italic' },
-		TelescopePromptBorder { NonText, gui = 'nocombine' },
+		TelescopeMatching { Identifier, fg = cerulean.lighten(20), gui = "italic" },
+		TelescopePromptBorder { NonText, gui = "nocombine" },
 		TelescopeSelection {},
 		-- TelescopeMultiSelection { Search },
 		TelescopeSelectionCaret { TelescopePromptBorder },
 		TelescopePromptPrefix {},
 		TelescopeResultsBorder {},
 		TelescopePreviewBorder {},
-		TelescopePreviewLine { Search, gui = 'inverse' },
+		TelescopePreviewLine { Search, gui = "inverse" },
 
 		-- gitsigns
 		GitSignsAdd { fg = DiffAdd.bg },
@@ -146,6 +153,31 @@ local theme = lush(function()
 		CodeBlock { bg = bg.lighten(5) },
 		HeadLine { bg = bg.lighten(10) },
 
+		-- Treesitter
+		sym "@constructor" { Special },
+		sym "@punctuation" { Punctuation },
+		sym "@punctuation.bracket" { Punctuation },
+		sym "@punctuation.delimiter" { Punctuation },
+		sym "@punctuation.special" { Punctuation },
+		sym "@symbol" { Constant },
+		sym "@constant" { Constant },
+		sym "@constant.builtin" { Keyword },
+		sym "@string.escape" { Character },
+		sym "@method" { Method },
+		sym "@function" { Method },
+		sym "@function.call" { Method },
+		sym "@function.builtin" { Method },
+
+		-- HTML
+		sym "@tag.delimiter.html" { fg = skyBlue },
+		sym "@tag.attribute.html" { Identifier },
+
+		-- Svelte
+		sym "@tag.delimiter.svelte" { fg = skyBlue },
+		sym "@tag.attribute.svelte" { Identifier },
+
+		-- Mason
+		MasonNormal { NormalFloat },
 	}
 end)
 return theme
